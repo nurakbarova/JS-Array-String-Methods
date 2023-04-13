030026f81e552d47aa42fc90dc61a056ddc016dc
nurakbarova <nurayakbarova76@gmail.com>
2023-04-14 01:02:30 +0400

    JS Array, String Methods

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// //    TASK1
+// let data = [
+//     {name: 'daniel', age: 45},
+//     {name: 'john', age: 34},
+//     {name: 'robert', age: null},
+//     {name: 'jen', age: undefined},
+//     {name: null, age: undefined}];
+//     let filtirEmplot=data.filter((data)=>data.age!= null)
+    
+//     console.log(filtirEmplot)
+
+// // TASK2
+// let students1 = {
+//     name: "Leyla",
+//     points: 500,
+// };
+// let students2 = {
+//     name: "Akif",
+//     points: 500,
+// };
+
+// let students3 = {
+//     name: "Fikret",
+//     points: 3000
+// };
+// let students4 = {
+//     name: "Rashad",
+//     points: 500
+// };
+// let students5 = {
+//     name: "Turkan",
+//     points: 1900,
+// };
+// let students6 = {
+//     name: "Sabir",
+//     points: 1000
+// };
+// let students7 = {
+//     name: "Idris",
+//     points: 340
+// };
+// let students8 = {
+//     name: "Nijat",
+//     points: 940
+// };
+//     let students9 = {
+//         name: "Ismet",
+//         points: 1780
+//     };
+
+
+
+//     let students = [students1, students2, students3, students5, students6,students7,students8,students9];
+// students.map((students) => {
+//     return students.points +=30;
+// })
+// console.log(students);
